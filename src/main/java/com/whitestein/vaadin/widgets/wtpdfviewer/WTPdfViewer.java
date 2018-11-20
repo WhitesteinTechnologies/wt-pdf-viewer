b5979c02c8351110357c74fabe47f09356eb19ea
Maria Jurcovicova <mna@whitestein.com>
2018-11-20 13:24:58 +0100

    Sometimes client side attempts to switch between multiple different pdfs quickly, quicker then the pdf can load. This can lead to errors or it can confuse the server side to send wrong stream as a response to older request.
    1.) Throw exception only if the last of concurrent file downloads failed.
    2.) If the download is obsolete before it even started, send nothing.

--- src/main/java/com/whitestein/vaadin/widgets/wtpdfviewer/WTPdfViewer.java
+++ src/main/java/com/whitestein/vaadin/widgets/wtpdfviewer/WTPdfViewer.java
@@ -1,7 +1,17 @@
 package com.whitestein.vaadin.widgets.wtpdfviewer;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.vaadin.annotations.JavaScript;
+import com.vaadin.server.ConnectorResource;
+import com.vaadin.server.DownloadStream;
 import com.vaadin.server.StreamResource;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinResponse;
+import com.vaadin.server.VaadinSession;
 import com.whitestein.vaadin.widgets.wtpdfviewer.client.WTPdfViewerClientRpc;
 import com.whitestein.vaadin.widgets.wtpdfviewer.client.WTPdfViewerServerRpc;
 import com.whitestein.vaadin.widgets.wtpdfviewer.client.WTPdfViewerState;
@@ -12,6 +22,7 @@ import com.whitestein.vaadin.widgets.wtpdfviewer.client.WTPdfViewerState;
 public class WTPdfViewer extends com.vaadin.ui.AbstractComponent {
 
 	private static final long serialVersionUID = -8979560729307918782L;
+	private static final Logger log = LoggerFactory.getLogger(WTPdfViewer.class);
 
 	// To process events from the client, we implement and register ServerRpc
 	private WTPdfViewerServerRpc rpc = new WTPdfViewerServerRpc() {
@@ -31,6 +42,10 @@ public class WTPdfViewer extends com.vaadin.ui.AbstractComponent {
 
 	public void setResource(StreamResource sourceFile) {
 		setResource("resourceFile", sourceFile);
+	}
+
+	public StreamResource getResource() {
+		return (StreamResource) getResource("resourceFile");
 	}
 
 	public void firstPage() {
@@ -62,4 +77,79 @@ public class WTPdfViewer extends com.vaadin.ui.AbstractComponent {
 		WTPdfViewerClientRpc proxy = getRpcProxy(WTPdfViewerClientRpc.class);
 		proxy.setShowPreviousViewOnLoad(showPreviousViewOnLoad);
 	}
+	
+    @Override
+    public boolean handleConnectorRequest(VaadinRequest request, VaadinResponse response, String path) throws IOException {
+        DownloadStream stream = null;
+        String[] parts = path.split("/", 2);
+        String key = parts[0];
+        String requestedFilename = parts[1];
+
+        VaadinSession session = getSession();
+        session.lock();
+        try {
+            ConnectorResource resource = (ConnectorResource) getResource(key);
+            if (resource == null) {
+                return false;
+            }
+            stream = resource.getStream();
+        } finally {
+            session.unlock();
+        }
+        	
+        try {
+        	/**
+        	 * When client side attempts to switch between multiple different pdfs quickly, quicker then the pdf can load,  
+        	 * following order of events can happen:
+        	 * setResource("one.pdf");
+        	 * setResource("two.pdf");
+        	 * 
+        	 * handleConnectorRequest(..., one.pdf); <- in this case one.pdf resource is no longer available, only two.pdf is
+        	 * 										 <- it is better to return nothing and let client show error then return wrong pdf 
+        	 *                                       <- however in pdf viewer we expect the second handleConnectorRequest to come shortly
+        	 * handleConnectorRequest(..., two.pdf);
+        	 */
+        	if (requestedFilename.equals(stream.getFileName())) {
+            	stream.writeResponse(request, response);
+        	} else {
+            	log.warn("Obsolete pdf file requested. The stream is not available anymore, sending nothing.");
+            	log.warn("Requested filename: " + requestedFilename + " available filename: " + stream.getFileName());
+        	}
+        } catch (IOException ex) {
+        	/**
+        	 * When client side attempts to switch between multiple different pdfs quickly, quicker then the pdf can load,  
+        	 * the exception could happen in any of the files. Most error happens because client side aborted old requests
+        	 * and issued new request for newer pdf. In that case, we can safely ignore the error and just log it.
+        	 */
+        	String currentFilename = currentFilenameForErrorHanding(session);
+        	if (isEqual(requestedFilename, currentFilename)) {
+        		// This is case when last request failed and pdf in browser has nothing to show. Throw
+        		// the exception so that the reset of application error handler can act on it. 
+            	throw ex;
+        	} 
+        	
+        	log.warn("Exception while downloading file. The file was already obsolete, ignoring exception.", ex);
+        	log.warn("File that failed: " + requestedFilename + " current filename: " + stream.getFileName());
+        }
+        return true;
+    }
+
+	private boolean isEqual(String str1, String str2) {
+		if (str1 == null) {
+			return str2 == null;
+		}
+		return str1.equals(str2);
+	}
+
+	protected String currentFilenameForErrorHanding(VaadinSession session) {
+		session.lock();
+		try {
+			StreamResource currentResource = getResource();
+			String currentFilename = currentResource==null ? null :currentResource.getFilename();
+			return currentFilename;
+		} finally {
+		    session.unlock();
+		}
+	}
+
 }
